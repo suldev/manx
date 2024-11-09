2a55442fee55b28308fcae3cc0ff2df3844ffaf8
suldev <suleyk.dev@gmail.com>
2024-11-09 10:17:30 -0500

    cleaned up help description

--- src/main.py
+++ src/main.py
@@ -5,14 +5,14 @@ def main():
     syntax = {'local' : 0, 'server' : 1, 'hosts' : 2, 'domain' : 3, 'adblock' : 4}
     parser = argparse.ArgumentParser(
         prog='manx',
-        description='Combines multiple blocklists into a single dnsmasq configuration file. This program is currently limited to dnsmasq blocklists since 2.86'
+        description='Combines multiple blocklists into a single dnsmasq configuration file.'
     )
     parser.add_argument('path', type=argparse.FileType('r'), metavar='FILE', help='Required. New-line delimited list of urls. Use # for comments')
     parser.add_argument('-i', '--install', default=False, action='store_true', help='Install the configuration file and restart dnsmasq. Must be run as root.')
-    parser.add_argument('-o', '--output', type=argparse.FileType('w'), metavar='FILE', default='blacklist.conf', help='Output file name. Defaults to blocklist.conf')
+    parser.add_argument('-o', '--output', type=argparse.FileType('w'), metavar='FILE', default='blacklist.conf', help='Output file name. Defaults to $PWD/blocklist.conf')
     parser.add_argument('-T', default='%Y-%m-%d %H:%M:%S', metavar='FORMAT', help='Set the time stamp formatting using python standard strftime format. Default is ISO8601 format. Not used for -x')
-    parser.add_argument('-v', '--verbose', default=False, action='store_true', help='Print all steps.')
-    parser.add_argument('-x', '--nohead', default=False, action='store_true', help='Do not printer header information (program name, version, time, etc.).')
+    parser.add_argument('-v', '--verbose', default=False, action='store_true', help='Print debugging information.')
+    parser.add_argument('-x', '--nohead', default=False, action='store_true', help='Do not print header information (program name, version, time, etc.).')
     parser.add_argument('-s', '--syntax', default='local', metavar='KEY', help='Set the output syntax. Options are local, server, hosts, adblock, and domain.')
 
     wl_arg_grp = parser.add_argument_group('Whitelist', 'Provide a newline-separated list of urls to whitelist')
